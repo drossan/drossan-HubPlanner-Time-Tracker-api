78a740844cb70830c0f6762fd4e03d3686d17864
Daniel Rosselló <daniel.rossello@secuoyas.com>
2024-08-05 15:56:59 +0200

    refactor: Add MakeHTTPRequest function in helpers package

---
+++ helpers/api_request.go
@@ -0,0 +1,31 @@
+package helpers
+
+import (
+	"io"
+	"net/http"
+)
+
+func MakeHTTPRequest(method, url, apiToken, contentType string, body io.Reader) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", apiToken)
+	req.Header.Add("Content-Type", contentType)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(res.Body)
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bodyBytes, nil
+}
